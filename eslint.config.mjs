44906a1e018e2ebb214a5ab6437eb5eb6e0adb14
Kushagra Behal <9810175011vikky@gmail.com>
2025-02-25 21:58:55 +0530

    es lint rules off

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -14,7 +14,7 @@ const eslintConfig = [
   ...compat.config({
     extends: ['next'],
     rules: {
-      "@typescript-eslint/no-explicit-any": "error"
+      "@typescript-eslint/no-explicit-any": "off"
     },
   }),
 ];
