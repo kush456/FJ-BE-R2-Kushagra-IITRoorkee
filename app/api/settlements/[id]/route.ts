7aebbac637054cc072dc08b95a9039875af8b2b5
Kushagra Behal <9810175011vikky@gmail.com>
2025-09-18 22:47:41 +0530

    deployement fixes

--- app/api/settlements/[id]/route.ts
+++ app/api/settlements/[id]/route.ts
@@ -1,4 +1,4 @@
-import { NextRequest, NextResponse } from 'next/server';
+import { NextResponse } from 'next/server';
 import { prisma } from '@/lib/prisma';
 
 // PATCH /api/settlements/[id] - mark as paid
